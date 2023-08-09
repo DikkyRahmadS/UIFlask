aeba90a9a0bcd25f2efc88199f6bc261d482044d
DikkyRahmadS <dikkyrahmads@gmail.com>
2023-08-09 14:35:51 +0700

    membuat ui login dan register

---
+++ app.py
@@ -0,0 +1,156 @@
+from flask import Flask  , render_template,request,make_response,jsonify,url_for,redirect,Response
+from sqlalchemy import create_engine,and_
+from flask_sqlalchemy import SQLAlchemy
+import logging
+from functools import wraps
+import jwt
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+
+
+# JUST IN CASE, Connect db without ORM.
+
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = ''
+# app.config['MYSQL_DB'] = 'flask_proto'
+ 
+# mysql = MySQL(app)
+
+# JWT Secret Key
+app.config['SECRET_KEY'] = 'your secret key'
+
+# To connect db
+app.config['SQLALCHEMY_DATABASE_URI'] ="mysql://root:@localhost/flask_proto"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class user(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(30))
+    password = db.Column(db.String(20))
+ 
+ 
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+# Validate token
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # jwt is passed in the request header
+
+
+        if 'access_token' in request.cookies:
+            token = request.cookies.get('access_token')
+        # return 401 if token is not passed
+        if not token:
+            return redirect("/login")
+  
+        try:
+            decoded_data = jwt.decode(jwt=token,
+                              key=app.config['SECRET_KEY'],
+                              algorithms=["HS256"])
+            current_user = user.query.filter_by(id=decoded_data["userid"]).first()
+
+        except:
+            return jsonify({
+                'message' : 'Token is invalid !!'
+            }), 401
+        # returns the current logged in users context to the routes
+        return  f( *args, **kwargs)
+        # return
+  
+    return decorated
+
+
+# User Route
+@app.route("/user",methods=['GET'])
+@token_required
+def userRoute():
+    if request.method == "GET":
+        token = request.cookies.get('access_token')
+        # return token
+        decoded_data = jwt.decode(jwt=token,
+                              key=app.config['SECRET_KEY'],
+                              algorithms=["HS256"])
+
+        
+        result = user.query.filter_by(id=decoded_data["userid"]).first()
+
+        # return decoded_data["userid"]
+        return render_template('user.html',user=result)
+
+# Main Route
+@app.route("/" , methods=['GET'])
+@token_required
+def index():
+    if request.method == "GET":
+        users = user.query.all()
+        return render_template('index.html', users = users)
+    
+@app.route("/register", methods=['GET', 'POST'])
+
+# Register Route (Unprotected)
+def register():
+    if request.method == "GET":
+        return render_template('register.html')
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        newUser = user(username=username, password=password)
+        db.session.add(newUser)
+        db.session.commit()
+        return f"Done!!"
+
+# Login Route (Unprotected)
+@app.route("/login",methods=['GET', 'POST'])
+def login():
+    if request.method == "GET":
+        return render_template('login.html')
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        result = user.query.filter_by(username=username).first()
+
+        if result :
+            if result.password == password:
+                # return result.username
+                token = jwt.encode({
+                'userid': result.id,
+                'exp' : datetime.utcnow() + timedelta(minutes = 30)
+                }, app.config['SECRET_KEY'])
+                # return "logged in"
+                response = make_response(redirect("/user"))
+                response.set_cookie('access_token',value=token)
+                return response
+                # return make_response(jsonify({'token' : token}), 201)
+                
+            else:
+                return 'Incorrect password, try again.'
+
+        else :
+            return "no user found"
+        
+
+        return redirect(url_for('login'))
+
+# Logout Route, clear cookie (Unprotected)
+@app.route("/logout",methods=['POST'])
+def logout():
+    if request.method == "POST":
+        userid = request.form['userid']
+        response = make_response(redirect("/"))
+        response.delete_cookie(key="access_token")
+        return  response
+        # Response.delete_cookie(key="access_token" )
+       
+        # return render_template()
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ db_schema/flask_proto.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 09, 2023 at 03:19 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `flask_proto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, '1', '1'),
+(3, '1', '1'),
+(4, '2', '2'),
+(6, 'kadal', 'duduk');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
